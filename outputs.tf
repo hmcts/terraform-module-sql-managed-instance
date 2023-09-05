1e2735fb6aa214e404571e4256af1357018ad5a9
salim abdulkareem <salim.abdulkareem@justice.gov.uk>
2023-09-05 13:23:34 +0100

    Comment it out directory roles

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,6 @@ output "resource_group_location" {
   value = local.sqlmi_location
 }
 
-output "roles" {
-  value = data.azuread_directory_roles.current.object_ids
-}
+# output "roles" {
+#   value = data.azuread_directory_roles.current.object_ids
+# }
