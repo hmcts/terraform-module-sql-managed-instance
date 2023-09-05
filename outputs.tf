7defef99db496094407e011ab13b11c434f1b7b4
salim abdulkareem <salim.abdulkareem@justice.gov.uk>
2023-09-05 13:32:53 +0100

    update

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
-output "resource_group_name" {
-  value = local.sqlmi_resource_group
-}
+# output "resource_group_name" {
+#   value = local.sqlmi_resource_group
+# }
 
-output "resource_group_location" {
-  value = local.sqlmi_location
-}
+# output "resource_group_location" {
+#   value = local.sqlmi_location
+# }
 
 # output "roles" {
 #   value = data.azuread_directory_roles.current.object_ids
