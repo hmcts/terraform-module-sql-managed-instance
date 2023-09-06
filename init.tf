d6fa6bda9b4cbeb2584527098fba7fc9348df106
Alex Bance <Alex.Bance@justice.gov.uk>
2023-09-06 13:56:38 +0100

    fix: added random and azuread as required providers

--- init.tf
+++ init.tf
@@ -4,5 +4,13 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.71.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.41.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
   }
 }
