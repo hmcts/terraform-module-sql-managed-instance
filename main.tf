42c6b61d40b9e262cbddf210490f3ee7782e3433
salim abdulkareem <salim.abdulkareem@justice.gov.uk>
2023-09-06 12:03:49 +0100

    change to DTS platform operation

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ resource "azurerm_mssql_managed_instance" "sqlmi" {
 resource "azurerm_mssql_managed_instance_active_directory_administrator" "sqlmi" {
   count                       = var.enable_read_only_group_access ? 1 : 0
   managed_instance_id         = azurerm_mssql_managed_instance.sqlmi.id
-  login_username              = "salim.abdulkareem@justice.gov.uk"
+  login_username              = "DTS Platform Operations"
   object_id                   = data.azuread_group.db_admin.object_id
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   depends_on = [
