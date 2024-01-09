40612c312c01ce0ea414a60cde533d0a839d1b8c
Simran Sian <simran.sian1@justice.gov.uk>
2024-01-09 09:26:33 +0000

    Update links to documentation

--- inputs-required.tf
+++ inputs-required.tf
@@ -1,6 +1,10 @@
 variable "license_type" {
   type        = string
-  description = "The type of license the Managed Instance will use"
+  description = "The type of license the Managed Instance will use."
+  validation {
+    condition = contains(["LicenseIncluded", "BasePrice"], var.license_type)
+    error_message = "Invalid value for license_type. Accepted values are: LicenseIncluded or BasePrice"
+  }
 }
 
 variable "sku_name" {
@@ -8,7 +12,7 @@ variable "sku_name" {
   description = "The SKU Name for the SQL Managed Instance."
   validation {
     condition     = contains(["gp_gen4", "gp_gen5", "gp_gen8im", "gp_gen8ih", "bc_gen4", "bc_gen5", "bc_gen8im", "bc_gen8ih"], lower(var.sku_name))
-    error_message = "Invalid valid for sku_name. Valid values are: GP_Gen4, GP_Gen5, GP_Gen8IM, GP_Gen8IH, BC_Gen4, BC_Gen5, BC_Gen8IM or BC_Gen8IH."
+    error_message = "Invalid valid for sku_name. Accepted values are: GP_Gen4, GP_Gen5, GP_Gen8IM, GP_Gen8IH, BC_Gen4, BC_Gen5, BC_Gen8IM or BC_Gen8IH."
   }
 }
 
