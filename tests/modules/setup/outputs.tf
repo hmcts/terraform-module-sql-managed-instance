f53bc7ef095cd6595bf6c877c83afbd07b492cb9
Simran Sian <simran.sian1@justice.gov.uk>
2024-01-15 15:30:37 +0000

    Add SQLMI tests

---
+++ tests/modules/setup/outputs.tf
@@ -0,0 +1,19 @@
+output "common_tags" {
+  value = module.common_tags.common_tags
+}
+
+output "resource_group" {
+  value = azurerm_resource_group.test_rg.name
+}
+
+output location {
+  value = azurerm_resource_group.test_rg.location
+}
+
+output vnet {
+  value = azurerm_virtual_network.test_vnet.name
+}
+
+output subnet {
+  value = azurerm_subnet.test_snet.id
+}
