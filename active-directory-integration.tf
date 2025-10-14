f608e1fe2b9a47e64035c5b60e634bd34af0a213
verinder4291 <Verinder.Dhillon@hmcts.net>
2025-10-14 18:33:12 +0100

    fix

--- active-directory-integration.tf
+++ active-directory-integration.tf
@@ -1,9 +1,10 @@
 resource "azurerm_mssql_managed_instance_active_directory_administrator" "sqlmi" {
-  count               = var.user_assigned_managed_identity_id != null ? 1 : 0
-  managed_instance_id = azurerm_mssql_managed_instance.sqlmi.id
-  login_username      = "cactus-group"
-  object_id           = data.azuread_group.db_admin.object_id
-  tenant_id           = data.azurerm_client_config.current.tenant_id
+  count                       = var.user_assigned_managed_identity_id != null ? 1 : 0
+  managed_instance_id         = azurerm_mssql_managed_instance.sqlmi.id
+  login_username              = "cactus-group"
+  object_id                   = data.azuread_group.db_admin.object_id
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  azuread_authentication_only = true
   depends_on = [
     azurerm_mssql_managed_instance.sqlmi
   ]
