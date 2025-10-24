76917f0a31416aca69a9f796e911eb30bec97551
verinder4291 <Verinder.Dhillon@hmcts.net>
2025-10-24 19:04:57 +0100

    fix

--- interpolated-defaults.tf
+++ interpolated-defaults.tf
@@ -4,7 +4,7 @@ locals {
   resource_group = var.existing_resource_group_name == null ? azurerm_resource_group.new[0].name : data.azurerm_resource_group.existing[0].name
   location       = var.existing_resource_group_name == null ? azurerm_resource_group.new[0].location : data.azurerm_resource_group.existing[0].location
   is_prod        = length(regexall(".*(prod).*", var.env)) > 0
-  admin_group    = local.is_prod ? "DTS Platform Operations SC" : "DTS Platform Operationsactus-group"
+  admin_group    = local.is_prod ? "DTS Platform Operations SC" : "DTS Platform Operations"
   db_reader_user = local.is_prod ? "DTS JIT Access ${var.product} DB Reader SC" : "DTS ${upper(var.business_area)} DB Access Reader"
   admin_password = var.admin_password == null ? random_password.password[0].result : var.admin_password
   valid_skus     = ["gp_gen4", "gp_gen5", "gp_gen8im", "gp_gen8ih", "bc_gen4", "bc_gen5", "bc_gen8im", "bc_gen8ih"]
