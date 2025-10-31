ba4c7ba9890a9dd66d06004f01b96c53365d0048
Alex Bance <Alex.Bance@justice.gov.uk>
2025-10-31 08:48:30 +0000

    fix: minimum vs required versions

--- init.tf
+++ init.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.47.0"
+      version = ">= 4.47.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.47.0"
+      version = ">= 2.47.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.5.1"
+      version = ">= 3.5.1"
     }
   }
 }
