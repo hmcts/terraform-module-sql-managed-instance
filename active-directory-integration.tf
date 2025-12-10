750235a9315e909a16332a2373e2446f4ef7dea5
ishdeepdulkoan <ishdeep.dulkoan@hmcts.net>
2025-12-10 17:46:46 +0000

    change login_username

--- active-directory-integration.tf
+++ active-directory-integration.tf
@@ -1,7 +1,7 @@
 resource "azurerm_mssql_managed_instance_active_directory_administrator" "sqlmi" {
   count                       = var.user_assigned_managed_identity_id != null ? 1 : 0
   managed_instance_id         = azurerm_mssql_managed_instance.sqlmi.id
-  login_username              = "DTS Platform Operations SC"
+  login_username              = "DTS Platform Operations"
   object_id                   = data.azuread_group.db_admin.object_id
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   azuread_authentication_only = false
